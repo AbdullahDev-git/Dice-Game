ae05820bbaf623186b5f2662ad3443ca89aee116
Abdulah <Abdullahnoordev@gmail.com>
2024-09-14 10:24:23 -0700

    first commit

---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,29 @@
+ import styled from "styled-components"
+
+ function RoleDice({currentDice, roleDice }) {
+ 
+  return (
+    <DiceContainer>
+        <div className="dice"  onClick={roleDice}>
+            <img src={`/public/dice_${currentDice}.png`} alt="" />
+        </div>
+        <p>Click on Dice to roll</p>
+    </DiceContainer>
+  )
+}
+export default RoleDice
+
+const DiceContainer = styled.div`
+margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .dice{
+        cursor: pointer;
+    }
+
+    p{
+        font-size: 24px;
+    }
+`
